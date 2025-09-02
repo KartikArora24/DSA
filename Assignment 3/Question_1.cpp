a076275a8bdfb154014c3407e27e69f2bb8e7ccc
KartikArora24 <arorakartik.ak01@gmail.com>
2025-09-03 01:59:12 +0530

    Update Question_1.cpp

--- Assignment 3/Question_1.cpp
+++ Assignment 3/Question_1.cpp
@@ -4,84 +4,74 @@
 #include<iostream>
 using namespace std;
 
-class Stack{
-    int *arr;
-    int size;
-    int topI;
+class stack{
+    int element[5];
+    int top=-1;
 
-    public:
-     Stack(){
-        size=10;
-        arr= new int[size];
-        topI=-1;
-     }
-     bool isFull(){
-        if(topI == size-1){
-            cout<<endl<<"Stack is full"<<endl;
-            return true;
-            
+public:
+    int isFull()
+    {
+        if (top==4) 
+            return 1;
+        else
+          return 0;
+ //Time Complexity: O(1);
+    }
+    void push(int x)
+    {
+        if(isFull())
+            cout<<"\n Overflow";
+        else
+            top++;
+            element[top]= x;
+    }
+ //Time Complexity: O(1);
+    void display()
+    {
+        for (int i=top;i>=0;i--)
+            cout<<element[i]<<endl;
+    }
+ //Time Complexity: O(N);
+    int isEmpty()
+    {
+        if(top==-1)
+            return 1;
+        else
+            return 0;
+    }
+ //Time Complexity: O(1);
+    void peek() 
+    {
+        if(isEmpty())
+        {
+            cout<<"Underflow";
         }
-
-        return false;
-        //Time Complexity: O(1);
-     }
-     void push(int x){
-       if(isFull()) return;
-
-        topI++;
-        arr[topI]=x;
-        //Time Complexity: O(1);
-        //Space Complexity: O(1);
+        else
+            cout<<element[top];
+    }
+ //Time Complexity: O(1);
+    void pop()
+    {
+        if(isEmpty())
+        {
+            cout<<"Underflow";
         }
-     bool isEmpty(){
-        if( topI == -1){
-            cout<<endl<<"Stack is empty"<<endl;
-            return true;
-            
-        }
-        return false;
-        //Time Complexity: O(1);
-     }
-     void pop(){
-        if(isEmpty()) return;
-        topI--;
-        //Time Complexity: O(1);
-     }
+        else
+            top--;
+    }
+ //Time Complexity: O(1);
 
-     int peek(){
-        if(isEmpty()) 
-        return -1;
-
-        return arr[topI];
-        //Time Complexity: O(1);
-     }
-
-     void Display(){
-        if(isEmpty()) return;
-        cout<<"Stack is: ";
-        for(int i=topI; i>=0; i--){
-              cout<<"\t"<<arr[i];
-        }
-        //Time Complexity: O(N);
-     }
 };
 
 
-int main(){
-    Stack st;
-    st.push(4);
-    st.push(7);
-    st.push(3);
-    st.push(2);
-    cout<<st.peek()<<endl;
-    st.pop();
-    st.Display();
-    st.pop();
-    cout<<st.isFull();
-    st.pop();
-    st.pop();
-    st.peek();
-    st.isEmpty();
-    
-    return 0;
+int main()
+{
+    stack S;
+    for (int i=0;i<=4;i++)
+    S.push(10+i);
+    S.display();
+    S.push(50);
+    //S.display();
+    cout<<"Top element is ";
+    S.peek();
 }
