9a118abe5d0fda5ada3a8f4d7b3a9abb449db0c9
KartikArora24 <arorakartik.ak01@gmail.com>
2025-11-24 00:50:02 +0530

    Add files via upload

---
+++ Assignment 7/Question_2.cpp
@@ -0,0 +1,30 @@
+/* Q2 Improved Selection Sort (min+max each pass)
+   Time: O(n²)
+   Space: O(1)
+*/
+#include <iostream>
+using namespace std;
+
+void improvedSelection(int a[], int n){
+    int start=0, end=n-1;
+    while(start<end){
+        int minIdx=start, maxIdx=start;
+        for(int i=start;i<=end;i++){
+            if(a[i]<a[minIdx]) minIdx=i;
+            if(a[i]>a[maxIdx]) maxIdx=i;
+        }
+        swap(a[start], a[minIdx]);
+        if(maxIdx==start) maxIdx=minIdx;
+        swap(a[end], a[maxIdx]);
+        start++; end--;
+    }
+}
+
+int main(){
+    int a[]={7,3,2,9,1,8}, n=6;
+    improvedSelection(a,n);
+    for(int i=0; i<n; i++)
+    cout << a[i] << " ";
+
+  return 0;
+}
